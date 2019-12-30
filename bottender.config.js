5c221fd7abcd42fd83671c34cf3a4f8644a340a4
JIAYU LIN <louis70109@gmail.com>
2019-12-30 23:23:16 +0800

    enable line

--- bottender.config.js
+++ bottender.config.js
@@ -32,7 +32,7 @@ module.exports = {
       verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
     },
     line: {
-      enabled: false,
+      enabled: true,
       path: '/webhooks/line',
       accessToken: process.env.LINE_ACCESS_TOKEN,
       channelSecret: process.env.LINE_CHANNEL_SECRET,
