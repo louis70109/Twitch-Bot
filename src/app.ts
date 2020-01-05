020af0ec118e6aba78a7108f21a466633569b45a
JIAYU LIN <louis70109@gmail.com>
2020-01-05 22:11:36 +0800

    add new condition

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,28 @@ import { platform, router, text } from 'bottender/router';
 import userBinding from './controller/users/binding';
 import userFollow from './controller/users/follow';
 import topGames from './controller/twitches/top';
+import searchGame from './controller/twitches/searchGame';
 import mongoose from 'mongoose';
 
-async function LineAction(context): Promise<void> {
+async function LineAction(): Promise<void> {
   return await router([
     text(/^綁定\s*(?<name>[\s\S]+)/, userBinding),
-    text(/^[f|F]ollow/, userFollow),
-    text(/top\s\s*(?<game>[\s\S]+)/, topGames),
+    text(/^([f|F]ollow)|追隨/, userFollow),
+    text(/([t|T]op)|遊戲/, topGames),
+    text(/^我[要|想]看\s*(?<topic>.+)$/, searchGame),
   ]);
 }
 
-async function MessengerAction(context): Promise<void> {
+async function MessengerAction(): Promise<void> {
   return await router([
     text(/^綁定\s*(?<name>[\s\S]+)/, userBinding),
-    text(/^[f|F]ollow/, userFollow),
-    text(/top\s\s*(?<game>[\s\S]+)/, topGames),
+    text(/^([f|F]ollow)|追隨/, userFollow),
+    text(/([t|T]op)|遊戲/, topGames),
+    text(/^我[要|想]看\s*(?<topic>.+)$/, searchGame),
   ]);
 }
 
-export default async function App(context): Promise<void> {
+export default async function App(): Promise<void> {
   mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -31,7 +34,8 @@ export default async function App(context): Promise<void> {
     platform('line', LineAction),
     platform('messenger', MessengerAction),
     text(/^綁定\s*(?<name>[\s\S]+)/, userBinding),
-    text(/^[f|F]ollow/, userFollow),
-    text(/top\s\s*(?<game>[\s\S]+)/, topGames),
+    text(/^([f|F]ollow)|追隨/, userFollow),
+    text(/([t|T]op)|遊戲/, topGames),
+    text(/^我[要|想]看\s*(?<topic>.+)$/, searchGame),
   ]);
 }
