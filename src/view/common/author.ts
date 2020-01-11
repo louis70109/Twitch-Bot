6cbffaa19ac7a27ea8f7f45ea82e440c1aa130ec
JIAYU LIN <louis70109@gmail.com>
2020-01-12 01:20:51 +0800

    update async

--- src/view/common/author.ts
+++ src/view/common/author.ts
@@ -1,6 +1,6 @@
 import sendMessage from './sendMessage';
-export default function author(context): void {
+export default async function author(context) {
   const message =
     '🦈【NiJia Lin】🦈\n✏️ 部落格:\nhttps://nijialin.com/\n\n🐙 Github:\nhttps://github.com/louis70109';
-  sendMessage(context, message);
+  await sendMessage(context, message);
 }
--- src/view/common/help.ts
+++ src/view/common/help.ts
@@ -1,7 +1,7 @@
 import sendMessage from './sendMessage';
 
-export default function helpMe(context): void {
+export default async function helpMe(context) {
   const message =
     '🎮 輸入範例\n綁帳號: 綁定 sam1268\n查詢追隨: follow\n找遊戲: top';
-  sendMessage(context, message);
+  await sendMessage(context, message);
 }
