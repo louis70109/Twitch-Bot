c597cdeffedf61054b70937c6e592613f854af45
NiJia <louis70109@gmail.com>
2020-02-23 21:42:07 +0800

    add express

---
+++ server.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const { bottender } = require('bottender');
+const mongoose = require('mongoose');
+
+const app = bottender({
+  dev: process.env.NODE_ENV !== 'production',
+});
+
+const port = Number(process.env.PORT) || 5000;
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(
+    bodyParser.json({
+      verify: (req, _, buf) => {
+        req.rawBody = buf.toString();
+      },
+    })
+  );
+
+  server.get('/send-id', (req, res) => {
+    res.json({ id: process.env.LINE_LIFF_ID });
+  });
+
+  server.get('/liff', (req, res) => {
+    const filename = path.join(`${__dirname}/liff.html`);
+    res.sendFile(filename);
+  });
+
+  // delegate other requests to bottender
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    mongoose.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    mongoose.Promise = global.Promise;
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
