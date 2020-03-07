71d172e4956ff92d04aed95d21e8abcac94255de
NiJia <louis70109@gmail.com>
2020-03-07 18:01:13 +0800

    feat: 處理使用 Notify  callback 是 LIFF 的參數問題

--- src/controller/notifiesController.ts
+++ src/controller/notifiesController.ts
@@ -1,28 +1,36 @@
 import axios from 'axios';
-import qs from 'querystring';
+import querystring from 'querystring';
 import { NotifyModel } from '../model/notify';
 class NotifyController {
-  static async confirmNotify(req, res) {
-    const line_code = req.query.code;
-    const userId = req.query.state;
-    const client = qs.stringify({
+  static confirmNotify(req, res): void {
+    const query = req.query['liff.state'].split('?')[1].split('&');
+    const notifyPayload = { code: null, state: null };
+    query.forEach(el => {
+      const queryObj = el.split('=');
+      if (queryObj[0] in notifyPayload)
+        notifyPayload[queryObj[0]] = queryObj[1];
+    });
+    const code = notifyPayload.code;
+    const userId = notifyPayload.state;
+    const client = {
       grant_type: 'authorization_code',
-      code: line_code,
+      code,
       redirect_uri: process.env.REDIRECT_URI,
       client_id: process.env.CLIENT_ID,
       client_secret: process.env.CLIENT_SECRET,
-    });
-    await axios({
-      method: 'post',
-      url: 'https://notify-bot.line.me/oauth/token',
-      data: client,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    })
+    };
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+    axios
+      .post(
+        'https://notify-bot.line.me/oauth/token',
+        querystring.encode(client),
+        { headers }
+      )
       .then(response => {
         NotifyModel.findOne({ userId: userId }, (err, isAlive) => {
-          const notify: any = {} as typeof NotifyModel;
+          const notify: any = NotifyModel();
           notify.token = response.data.access_token;
           notify.userId = userId;
           if (!isAlive) {
@@ -45,13 +53,13 @@ class NotifyController {
         });
         res.render('notify_confirm', {
           message: response.data,
-          liffId: process.env.LIFF_ID,
+          liffId: process.env.LIFF_ID_CALLBACK,
         });
       })
       .catch(err => {
         res.render('notify_confirm', {
-          message: err,
-          liffId: process.env.LIFF_ID,
+          message: err.message,
+          liffId: process.env.LIFF_ID_CALLBACK,
         });
       });
   }
