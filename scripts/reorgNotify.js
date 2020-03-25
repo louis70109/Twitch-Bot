87d0ec2dc38c08062ec823dbb46155f923c92fad
NiJia <louis70109@gmail.com>
2020-03-25 20:51:05 +0800

    chore: change function call to IIFE

--- scripts/reorgNotify.js
+++ scripts/reorgNotify.js
@@ -9,14 +9,12 @@ mongoose.set('useUnifiedTopology', true);
 URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TwitchBot';
 mongoose.connect(URI).then(() => (mongoose.Promise = global.Promise));
 
-async function run() {
+(async function run() {
   const now = moment(),
     hour = now.hours(),
     min = now.minutes();
 
   if ((hour === 8 || hour === 20) && min >= 0)
     await StreamNotifyModel.updateMany({}, { $set: { isPublish: false } });
-
   await mongoose.connection.close();
-}
-run();
+})()
