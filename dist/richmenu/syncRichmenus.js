75d6f757cae3a377334ce5e11609843f401e74c0
NiJia <louis70109@gmail.com>
2020-03-31 10:22:37 +0800

    chore: modify readme

---
+++ dist/richmenu/syncRichmenus.js
@@ -0,0 +1,79 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// https://github.com/Yoctol/bottender/blob/master/packages/bottender/src/cli/providers/line/menu.js
+var fs_1 = __importDefault(require("fs"));
+var messaging_api_line_1 = require("messaging-api-line");
+var bottender_config_1 = __importDefault(require("../bottender.config"));
+var client = messaging_api_line_1.LineClient.connect(bottender_config_1.default.channels.line);
+(function () { return __awaiter(void 0, void 0, void 0, function () {
+    var createdRichMenus, i, bMenuId;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, client.getRichMenuList()];
+            case 1:
+                createdRichMenus = _a.sent();
+                i = 0;
+                _a.label = 2;
+            case 2:
+                if (!(i < createdRichMenus.length)) return [3 /*break*/, 5];
+                // eslint-disable-next-line no-await-in-loop
+                return [4 /*yield*/, client.deleteRichMenu(createdRichMenus[i].richMenuId)];
+            case 3:
+                // eslint-disable-next-line no-await-in-loop
+                _a.sent();
+                _a.label = 4;
+            case 4:
+                i++;
+                return [3 /*break*/, 2];
+            case 5:
+                import richmenu from './richmenu';
+                return [4 /*yield*/, client.createRichMenu(richmenu_1.default)];
+            case 6:
+                bMenuId = (_a.sent()).richMenuId;
+                return [4 /*yield*/, client.uploadRichMenuImage(bMenuId, fs_1.default.readFileSync(richmenuB.meta.image.path))];
+            case 7:
+                _a.sent();
+                console.log({ bMenuId: bMenuId });
+                return [2 /*return*/];
+        }
+    });
+}); })();
