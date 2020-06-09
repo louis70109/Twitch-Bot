6c23f48c992bc78cfcf070f03ebebcab58f8f049
NiJia Lin <louis70109@gmail.com>
2020-06-09 17:36:57 +0800

    Updated: add help and author page IconSwtich

--- src/templates/common/author.ts
+++ src/templates/common/author.ts
@@ -1,7 +1,9 @@
 import { LineContext } from 'bottender';
+import { randomSticker } from '../../utils/IconSwitch';
 
 export default async function author(context: LineContext): Promise<void> {
   const message =
     '🦈【NiJia Lin】🦈\n✏️ 部落格:\nhttps://nijialin.com/\n\n🐙 Github:\nhttps://github.com/louis70109';
-  await context.sendText(message);
+  const sender = randomSticker();
+  await context.sendText(message, { sender });
 }
--- src/templates/common/help.ts
+++ src/templates/common/help.ts
@@ -1,7 +1,9 @@
 import { LineContext } from 'bottender';
+import { randomSticker } from '../../utils/IconSwitch';
 
 export default async function helpMe(context: LineContext): Promise<void> {
   const message =
     '🎮 輸入範例\n1.綁帳號: 綁定 sam1268\n2.查詢追隨: follow\n3.找遊戲: top\n4.輸入: 連結 LINE Notify\n5. 關於我: author';
-  await context.sendText(message);
+  const sender = randomSticker();
+  await context.sendText(message, { sender });
 }
